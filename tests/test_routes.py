bd6cca45826132364986f3a7b749d3e9292dedb4
dwreeves <xdanielreeves@gmail.com>
2022-04-21 16:34:54 -0400

    more coverage

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -29,18 +29,27 @@ def request_response_pairs(*files: str) -> callable:
 
 @request_response_pairs(
     "e1_m1_basic_example",
-    "e1_m1_non_international_us",
-    "e1_m1_non_international_china",
-    "e1_m1_two_predictions"
+    "e1_m1_two_predictions",
 )
-@pytest.mark.asyncio
-async def test_endpoint_api_v1_model_v1(client, request_response, db):
+def test_endpoint_api_v1_model_v1(client, request_response, db):
     req, expected_res = request_response
-    res = client.post("/api/v1/predict", json=req)
+    res = client.post("/api/v1/predict?model=v1", json=req)
     assert res.status_code == 200
     assert res.json() == expected_res
 
 
-@pytest.mark.asyncio
-async def test_health_check(client):
+@request_response_pairs(
+    "e1_m2_basic_example",
+    "e1_m2_non_international_us",
+    "e1_m2_non_international_china",
+    "e1_m2_two_predictions"
+)
+def test_endpoint_api_v1_model_v2(client, request_response, db):
+    req, expected_res = request_response
+    res = client.post("/api/v1/predict?model=v2", json=req)
+    assert res.status_code == 200
+    assert res.json() == expected_res
+
+
+def test_health_check(client):
     assert client.get("/healthz").status_code == 200
